22e30f822d255a422a2d7d8c6b535accd19f3006
matthsworld <matthsworld@gmail.com>
2024-12-20 20:40:34 +0100

    add: added main.ts file

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,29 @@
+import { Cpp, Java, React } from './subjects';
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+export const cTeacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
+};
+
+// Cpp Subject
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java Subject
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React Subject
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
